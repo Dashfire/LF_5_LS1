1d07450e87644e2002991d3ece2d414e59fa885e
Marc Czapp <73938214+Dashfire@users.noreply.github.com>
2024-01-16 12:39:12 +0100

    Update blink.cpp

--- blink_lib/blink.cpp
+++ blink_lib/blink.cpp
@@ -1,1 +1,31 @@
+#include "Arduino.h"
+#include "BlinkLED.h"
 
+/**
+Written by: MCzapp 16.01.2024
+Release: 1.1.0
+Version: 1.0.0
+
+Lets the LED Blink in the givin interval
+
+Input:
+  int Pin = Pin where your LED is located at.
+  int onTime = Time in ms that the LED is on HIGH
+  int offTime = Time in ms that the LED is on LOW
+
+Output:
+  void
+**/
+
+void blink (int pin, int onTime, int offTime)
+{
+
+  if(millis() - privousMilli > onTime) {
+    digitalWrite(pin, HIGH);
+  }
+
+  if(millis() - privousMilli > offTime) {
+    privousMilli = millis();
+    digitalWrite(pin,LOW);
+  }
+}
