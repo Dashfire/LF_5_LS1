506f5d5c5ab67e4a738af1cbe3d482c0cb5fbef7
Marc Czapp <73938214+Dashfire@users.noreply.github.com>
2024-02-05 10:27:27 +0100

    Update lib.ino

--- lib/lib.ino
+++ lib/lib.ino
@@ -1,1 +1,31 @@
+void setDisplay(float humid, float temp) {
 
+  lcd.clear();                //LCD Display clearen         
+
+  lcd.setCursor(0,0);
+  lcd.print("Humidity:");    
+
+  //Kontrollmechanismus für das richtige Platzieren des Cursors
+  if(humid >= 100) {
+    lcd.setCursor(9,0);
+  } else {
+    lcd.setCursor(10,0);
+  }
+
+  lcd.print(String(humid) + "%");
+
+  lcd.setCursor(0,1);
+  lcd.print("Temp:");
+
+  //Kontrollmechanismus für das richtige Platzieren des Cursors
+  if(temp <= -10) {
+    lcd.setCursor(9,1);
+  } else if(temp >= 0 && temp < 10) {
+    lcd.setCursor(11,1);
+  } else {
+    lcd.setCursor(10,1);
+  }
+
+  lcd.print(String(temp) + "C");
+  
+}
